aa622a36ba25b960d0bfa66f8464e11e908b89de
Adil Gabdullin <izenbard@gmail.com>
2022-12-28 00:37:59 +0600

    config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === "production"
-        ? ""
+        ? "/bim/dxf"
         : "/",
     transpileDependencies: [
         /[\\\/]node_modules[\\\/]dxf-viewer[\\\/]/
